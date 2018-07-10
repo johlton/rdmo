bd74692382f314ad3f60b318029760462b8ad7e7
Olaf Michaelis <omichaelis@aip.de>
2018-07-10 14:55:58 +0200

    Add options import draft

--- rdmo/options/imports.py
+++ rdmo/options/imports.py
@@ -2,7 +2,7 @@ import logging
 
 from django.core.exceptions import ValidationError
 
-from rdmo.core.imports import make_bool, get_value_from_treenode
+from rdmo.core.imports import get_savelist_setting, get_value_from_treenode, make_bool, model_will_be_imported
 from rdmo.core.utils import get_ns_map, get_ns_tag, get_uri
 
 from .models import OptionSet, Option
@@ -11,47 +11,57 @@ from .validators import OptionSetUniqueKeyValidator
 log = logging.getLogger(__name__)
 
 
-def import_options(optionsets_node):
+def import_options(optionsets_node, savelist={}, do_save=False):
     log.info('Importing options')
     nsmap = get_ns_map(optionsets_node.getroot())
 
     for optionset_node in optionsets_node.findall('optionset'):
-        uri = get_uri(optionset_node, nsmap)
+        optionset_uri = get_uri(optionset_node, nsmap)
 
         try:
-            optionset = OptionSet.objects.get(uri=uri)
+            optionset = OptionSet.objects.get(uri=optionset_uri)
+            optionset_before = optionset
         except OptionSet.DoesNotExist:
             optionset = OptionSet()
-            log.info('Optionset not in db. Created with uri ' + str(uri))
+            log.info('Optionset not in db. Created with uri ' + str(optionset_uri))
+            optionset_before = None
         else:
-            log.info('Optionset does exist. Loaded from uri ' + str(uri))
+            log.info('Optionset does exist. Loaded from uri ' + str(optionset_uri))
 
-        optionset.uri_prefix = uri.split('/options/')[0]
-        optionset.key = uri.split('/')[-1]
+        optionset.uri_prefix = optionset_uri.split('/options/')[0]
+        optionset.key = optionset_uri.split('/')[-1]
         optionset.comment = get_value_from_treenode(optionset_node, get_ns_tag('dc:comment', nsmap))
         optionset.order = get_value_from_treenode(optionset_node, 'order')
         try:
             OptionSetUniqueKeyValidator(optionset).validate()
         except ValidationError:
-            log.info('Optionset not saving "' + str(uri) + '" due to validation error')
+            log.info('Optionset not saving "' + str(optionset_uri) + '" due to validation error')
             pass
         else:
-            log.info('Optionset saving to "' + str(uri) + '"')
-            optionset.save()
+            savelist_uri_setting = get_savelist_setting(optionset_uri, savelist)
+            # update savelist
+            if optionset_before is None:
+                savelist[optionset_uri] = True
+            else:
+                savelist[optionset_uri] = model_will_be_imported(optionset_before, optionset)
+            # save
+            if do_save is True and savelist_uri_setting is True:
+                log.info('Optionset saving to "' + str(optionset_uri) + '"')
+                optionset.save()
 
         for options_node in optionset_node.findall('options'):
             for option_node in options_node.findall('option'):
-                uri = get_uri(option_node, nsmap)
+                option_uri = get_uri(option_node, nsmap)
 
                 try:
-                    option = Option.objects.get(uri=uri)
+                    option = Option.objects.get(uri=option_uri)
                 except Option.DoesNotExist:
                     log.info(Option.DoesNotExist)
                     option = Option()
 
                 option.optionset = optionset
-                option.uri_prefix = uri.split('/options/')[0]
-                option.key = uri.split('/')[-1]
+                option.uri_prefix = option_uri.split('/options/')[0]
+                option.key = option_uri.split('/')[-1]
                 option.comment = get_value_from_treenode(option_node, get_ns_tag('dc:comment', nsmap))
                 option.order = get_value_from_treenode(option_node, 'order')
 
@@ -62,8 +72,17 @@ def import_options(optionsets_node):
                 try:
                     OptionSetUniqueKeyValidator(optionset).validate()
                 except ValidationError:
-                    log.info('Optionset not saving "' + str(uri) + '" due to validation error')
+                    log.info('Optionset not saving "' + str(option_uri) + '" due to validation error')
                     pass
                 else:
-                    log.info('Option saving to "' + str(uri) + '"')
-                    option.save()
+                    savelist_uri_setting = get_savelist_setting(option_uri, savelist)
+                    # update savelist
+                    if optionset_before is None:
+                        savelist[option_uri] = True
+                    else:
+                        savelist[option_uri] = model_will_be_imported(optionset_before, optionset)
+                    # save
+                    if do_save is True and savelist_uri_setting is True:
+                        log.info('Option saving to "' + str(option_uri) + '"')
+                        option.save()
+    return savelist
--- rdmo/options/views.py
+++ rdmo/options/views.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from django.conf import settings
@@ -53,22 +54,48 @@ class OptionsImportXMLView(ModelPermissionMixin, ListView):
     permission_required = ('options.add_option', 'options.change_option', 'options.delete_option')
     success_url = reverse_lazy('options')
     parsing_error_template = 'core/import_parsing_error.html'
+    confirm_page_template = 'options/options_confirmation_page.html'
 
     def get(self, request, *args, **kwargs):
         return HttpResponseRedirect(self.success_url)
 
     def post(self, request, *args, **kwargs):
+        log.info('Validating post request')
+
+        # in case of receiving xml data
+        try:
+            savelist = json.loads(request.POST['tabledata'])
+        except KeyError:
+            pass
+        else:
+            log.info('Post seems to come from confirmation page')
+            return self.trigger_import(request, savelist, do_save=True)
+
+        # when receiving upload file
         try:
             request.FILES['uploaded_file']
-        except:
+        except Exception as e:
             return HttpResponseRedirect(self.success_url)
         else:
-            tempfilename = handle_uploaded_file(request.FILES['uploaded_file'])
+            log.info('Post from file import dialog')
+            request.session['tempfile'] = handle_uploaded_file(request.FILES['uploaded_file'])
+            return self.trigger_import(request)
 
-        roottag, xmltree = validate_xml(tempfilename)
+    def trigger_import(self, request, tabledata={}, do_save=False):
+        log.info('Parsing file ' + request.session.get('tempfile'))
+        roottag, xmltree = validate_xml(request.session.get('tempfile'))
         if roottag == 'options':
-            import_options(xmltree)
-            return HttpResponseRedirect(self.success_url)
+            savelist = import_options(xmltree, savelist=tabledata, do_save=do_save)
+            if do_save is False:
+                return self.render_confirmation_page(request, savelist=savelist)
+            else:
+                return HttpResponseRedirect(self.success_url)
         else:
             log.info('Xml parsing error. Import failed.')
             return render(request, self.parsing_error_template, status=400)
+
+    def render_confirmation_page(self, request, savelist, *args, **kwargs):
+        return render(request, self.confirm_page_template, {
+            'status': 200,
+            'savelist': sorted(savelist.items()),
+        })
